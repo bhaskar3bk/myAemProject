e9e77d623e4bee47d84e830b250f75bed78b2a1f
Bhaskar <bkumar15@sapient.com>
2017-05-14 18:50:36 -0400

    Initial version

---
+++ bundle/src/main/java/com/us/aem/project/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.us.aem.project;
+
+import aQute.bnd.annotation.Version;
---
+++ bundle/src/test/java/com/us/aem/project/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.us.aem.project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
